b6f891905c19a05a57e29280f06b06513ab2a6c6
ldupree99 <latrellddupree@gmail.com>
2023-12-11 18:05:49 -0500

    Added more detailed comments to Python. Updated function for more efficient run code

--- FCP2.py
+++ FCP2.py
@@ -1,7 +1,18 @@
 # Function to add an expense to the tracker
 def add_expense(expense_tracker, category, amount):
-    if category != expense_tracker:
+    """
+    Add an expense to the expense tracker.
+
+    Different Args added to function:
+    - expense_tracker (dict): Dictionary to the store expenses.
+    - category (str): Category of the expense.
+    - amount (float)
+    """
+    # Check if the category exist in the expense tracker
+    if category not in expense_tracker:
+        #If not, create an empty list for that category
         expense_tracker[category] = []
+    #Append the amount to the list of expenses for the specified category 
     expense_tracker[category].append(amount)
 
 # Initialize an empty expense tracker
@@ -18,8 +29,10 @@ add_fixed_expenses = input(f"Do you have entries for Fixed Expenses? (yes/no): "
 
 # Check user's response
 if add_fixed_expenses == 'yes':
+    #initialize the promt for adding another expense
     add_another_expense = 'yes'
 
+    #continue prompting the user as long as they want to add another expense
     while add_another_expense.lower() == 'yes':
         # Ask for expense details
         category = input("Enter the category for the expense: ")
@@ -31,9 +44,12 @@ if add_fixed_expenses == 'yes':
         # Ask if the user wants to add another expense
         add_another_expense = input("Do you want to add another expense? (yes/no): ")
 
+
     # Print the expense tracker
     print("Expense Tracker:")
     for category, amounts in expense_tracker.items():
+        #Display the total amount for each category
         print(f"{category}: {sum(amounts)}")
 
+#Display a message when no more expenses are added
 print("No more expenses added. Thank you for using the Expense Tracker!")
---
+++ Myfirstwebsite/README.md
@@ -0,0 +1,6 @@
+# Added figma design for website
+View the Figma design files for this project:
+
+[Figma Design](paste-your-figma-link-here](https://www.figma.com/file/R9VJ9jYriB6FhmXlLbHMTT/FirstWebsite?type=design&mode=design&t=AVuxLSY2rT4Oy9Dl-1
+
+
